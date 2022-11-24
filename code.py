fe9ee4c59ab130de88fb659a2e961524fd7c9be1
Tim Boldt <tim.boldt@gmail.com>
2022-11-24 11:11:57 -0800

    Basic sparkline working from Adafruit IO

--- code.py
+++ code.py
@@ -1,10 +1,15 @@
 import time
 import board
 import displayio
+import os
+import socketpool
+import supervisor
 import terminalio
+import wifi
 from adafruit_display_text import label
 from adafruit_display_shapes.sparkline import Sparkline
 import adafruit_il0373
+import adafruit_requests
 
 font = terminalio.FONT
 black = 0x0000FF
@@ -33,6 +38,28 @@ display = adafruit_il0373.IL0373(
     highlight_color=red,
 )
 
+aio_username = os.getenv("AIO_USERNAME")
+aio_key = os.getenv("AIO_KEY")
+aio_feed = "finance.coinbase-btc-usd"
+decimals = 1
+
+print("Getting data...")
+
+pool = socketpool.SocketPool(wifi.radio)
+http = adafruit_requests.Session(pool)
+resp = http.get("http://io.adafruit.com/api/v2/{}/feeds/{}/data/chart?hours=48&resolution=10".format(
+    aio_username, aio_feed), headers={"X-AIO-Key": aio_key})
+json = resp.json()
+resp.close()
+
+title = json["feed"]["name"]
+latest = "NO DATA"
+if len(json["data"]) > 0 and len(json["data"][-1]) >= 2:
+    latest = round(float(json["data"][-1][1]), decimals)
+
+print("{}: {}".format(title, latest))
+print("{} data points".format(len(json["data"])))
+
 g = displayio.Group()
 
 bg = displayio.Bitmap(display.width, display.height, 1)
@@ -40,18 +67,15 @@ bg_palette = displayio.Palette(1)
 bg_palette[0] = 0xFFFFFF
 g.append(displayio.TileGrid(bg, pixel_shader=bg_palette, x=0, y=0))
 
-text_area = label.Label(font, text="Hello World!", color=black)
-text_area.x = 100
-text_area.y = 80
+text_area = label.Label(font, text="{}: {}".format(title, latest), color=black)
+text_area.x = 0
+text_area.y = 10
 g.append(text_area)
 
-graph = Sparkline(width=display.width, height=display.height, max_items=40, y_min=0, y_max=10, x=0, y=0, color=red)
-graph.add_value(3)
-graph.add_value(1)
-graph.add_value(4)
-graph.add_value(1)
-graph.add_value(5)
-graph.add_value(9)
+graph = Sparkline(width=display.width, height=display.height-10, max_items=len(json["data"]), x=0, y=10, color=black)
+for d in json["data"]:
+    graph.add_value(float(d[1]), False)
+graph.update()
 g.append(graph)
 
 display.show(g)
@@ -60,3 +84,4 @@ display.refresh()
 print("refreshed")
 
 time.sleep(180)
+supervisor.reload()

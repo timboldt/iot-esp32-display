98c51cfb67ac9ebfd389eac4dd47fc10618008ce
Tim Boldt <tim.boldt@gmail.com>
2022-11-28 19:00:21 -0800

    Basic feed graphs are working

--- big_epaper/big_epaper.ino
+++ big_epaper/big_epaper.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_GFX.h>
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <GxEPD2_BW.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
@@ -11,7 +12,62 @@
 #include "display.h"
 #include "graph.h"
 
+const size_t MAX_VALS = 256;
+
 WiFiMulti wifi;
+
+size_t fetch_data(WiFiClientSecure* client, const String& feed_name,
+                  float vals[], String* name) {
+    *name = "NO DATA";
+    if (!client) {
+        return 0;
+    }
+
+    size_t val_count = 0;
+    DynamicJsonDocument doc(32768);
+
+    HTTPClient https;
+    const String url = "https://io.adafruit.com/api/v2/" ADAFRUIT_IO_USERNAME
+                       "/feeds/" +
+                       feed_name + "/data/chart?hours=48&resolution=10";
+    https.addHeader("X-AIO-Key", ADAFRUIT_IO_KEY);
+    if (https.begin(*client, url)) {
+        int httpCode = https.GET();
+        if (httpCode > 0) {
+            // HTTP header has been send and Server response header has
+            // been handled
+            Serial.printf("HTTP GET... code: %d\n", httpCode);
+
+            if (httpCode == HTTP_CODE_OK ||
+                httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+                DeserializationError error =
+                    deserializeJson(doc, https.getString());
+
+                if (error) {
+                    Serial.print("Could not parse data from HTTP request: ");
+                    Serial.println(error.c_str());
+                    return 0;
+                }
+
+                JsonArray data = doc["data"];
+                val_count = min(MAX_VALS, data.size());
+                for (size_t i = 0; i < val_count; i++) {
+                    JsonArray elem = data[i];
+                    vals[i] = elem[1].as<float>();
+                }
+                *name = doc["feed"]["name"].as<String>();
+            }
+        } else {
+            Serial.printf("HTTPS GET failed, error: %s\n",
+                          https.errorToString(httpCode).c_str());
+        }
+        https.end();
+    } else {
+        Serial.printf("Failed to make HTTP call to %s", url.c_str());
+    }
+
+    return val_count;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -38,32 +94,6 @@ void loop() {
     WiFiClientSecure* client = new WiFiClientSecure;
     if (client) {
         client->setInsecure();  // skip verification
-        {
-            HTTPClient https;
-            const String feed_name = "tricolor-battery";
-            const String url = "https://io.adafruit.com/api/v2/" ADAFRUIT_IO_USERNAME "/feeds/" + feed_name + "/data/chart?hours=48&resolution=10";
-            https.addHeader("X-AIO-Key", ADAFRUIT_IO_KEY);
-            if (https.begin(*client, url)) {
-                int httpCode = https.GET();
-                if (httpCode > 0) {
-                    // HTTP header has been send and Server response header has
-                    // been handled
-                    Serial.printf("HTTP GET... code: %d\n", httpCode);
-
-                    if (httpCode == HTTP_CODE_OK ||
-                        httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-                        String payload = https.getString();
-                        Serial.println(payload);
-                    }
-                } else {
-                    Serial.printf("HTTPS GET failed, error: %s\n",
-                                  https.errorToString(httpCode).c_str());
-                }
-                https.end();
-            } else {
-                Serial.printf("Failed to make HTTP call to %s", url.c_str());
-            }
-        }
     } else {
         Serial.println("Failed to create TCP client.");
     }
@@ -72,48 +102,6 @@ void loop() {
     display.firstPage();
     do {
         display.fillScreen(GxEPD_WHITE);
-
-        float vals[] = {
-            16638.54190, 16669.98116, 16358.55164, 16861.55528, 16106.95631,
-            16563.23739, 16678.76226, 16864.04261, 15066.46833, 15913.20199,
-            15642.25906, 15020.62563, 15106.67964, 15024.92191, 16571.18351,
-            16001.98618, 15794.06545, 16655.78695, 16963.05022, 16961.88637,
-            16472.57971, 15428.88701, 15987.22679, 16418.53457, 16439.55000,
-            15221.72126, 16826.08964, 16209.60129, 15958.39792, 15822.10587,
-            16804.17842, 15008.03018, 16868.92679, 15075.79270, 15731.01944,
-            16317.67791, 15922.29704, 15565.26314, 16821.85772, 15466.74799,
-            15815.03529, 16651.00241, 15789.00090, 15270.61038, 16959.20242,
-            16985.61689, 16659.62916, 15629.26682, 16544.35313, 16799.81701,
-            16498.94905, 16924.70388, 16693.46236, 15080.88698, 16813.28603,
-            15383.52871, 16195.50436, 15548.65688, 15361.32341, 16884.41586,
-            15451.67901, 15481.44128, 16756.79224, 16367.57833, 15686.24130,
-            16537.19697, 15493.40588, 15977.54016, 16302.29786, 16256.97954,
-            16900.87430, 15893.32913, 15428.39823, 16470.98999, 16874.98074,
-            15741.55647, 15552.69214, 15475.30586, 16428.95662, 15947.34984,
-            15052.69041, 16043.89888, 15838.52323, 16325.77347, 15748.91923,
-            16125.01253, 15937.78421, 16640.41191, 15961.16713, 16377.07830,
-            15278.38841, 15408.43163, 16135.90296, 15985.93070, 15169.83181,
-            15924.77735, 15207.59864, 15737.99671, 16139.15736, 16287.47645,
-            15932.63431, 15198.41265, 16675.50752, 15446.39062, 15180.52042,
-            15840.38413, 16082.44095, 16041.47588, 16908.04800, 15680.01461,
-            15254.90406, 16595.19578, 15248.37684, 15427.13633, 16360.05981,
-            15899.92842, 15879.77037, 15517.57411, 15828.17007, 15350.67638,
-            16390.97764, 16587.67455, 15212.86824, 15489.11097, 15827.05341,
-            16179.85523, 16278.08307, 16260.96886, 15168.61698, 15482.89906,
-            16577.64351, 16963.09394, 15884.92830, 15210.17204, 15286.83593,
-            16391.47510, 16094.49346, 15039.58979, 16704.56755, 15227.40489,
-            16943.46137, 15781.58728, 16127.58022, 15988.62103, 15151.21191,
-            16941.52656, 16225.15019, 15281.36213, 16845.44770, 15846.53129,
-            15205.13848, 16942.46580, 15546.07706, 15220.32132, 15772.11656,
-            15446.51592, 16439.52067, 15452.14754, 16345.99264, 16687.62291,
-            15125.02955, 16171.69344, 15348.24911, 16978.22548, 16538.40916,
-            15113.05242, 15933.21555, 16374.36491, 15309.12229, 16335.51548,
-            15960.29305, 16528.36029, 15797.15033, 16567.00798, 15113.32483,
-            16252.89691, 15708.13886, 15533.01495, 15553.98058, 15131.76169,
-            16186.31118, 15474.97588, 16476.13223, 15918.37202, 16181.02312,
-            16921.87185, 16114.56106, 15345.68468, 15298.72128, 16704.79397,
-            15368.69832, 16003.28265, 16999.66897, 15254.85826, 15090.09817,
-            16750.02748, 16103.80357, 15443.05650, 16526.24101, 16945.87675};
 
         String labels[] = {"",
                            "finance.coinbase-btc-usd",
@@ -133,11 +121,16 @@ void loop() {
                            "bigpaper-battery"};
         for (int16_t x = 0; x < 4; x++) {
             for (int16_t y = 0; y < 3; y++) {
+                Serial.printf("Processing graph (%d, %d)\n", x, y);
                 if (x == 0 && y == 0) {
                     // TODO: Show current date/time and voltage.
                 } else {
-                    draw_graph(labels[x + y * 4], x * 100, y * 100, 100, 100,
-                               200, vals);
+                    String name;
+                    float vals[MAX_VALS];
+                    size_t val_count =
+                        fetch_data(client, labels[x + y * 4], vals, &name);
+                    draw_graph(name, x * 100, y * 100, 100, 100,
+                               val_count, vals);
                 }
             }
         }

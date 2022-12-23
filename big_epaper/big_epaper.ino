67c9e0076e83aff15e6285e020ce94293fa93fe9
Tim Boldt <tim.boldt@gmail.com>
2022-12-23 11:18:41 -0800

    Move network logic to its own file

--- big_epaper/big_epaper.ino
+++ big_epaper/big_epaper.ino
@@ -1,15 +1,14 @@
 #include <Adafruit_GFX.h>
 #include <Arduino.h>
-#include <ArduinoJson.h>
 #include <Fonts/FreeSans9pt7b.h>
 #include <GxEPD2_BW.h>
-#include <HTTPClient.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <WiFiMulti.h>
 #include <stdint.h>
 
 #include "arduino_secrets.h"
+#include "adafruit_io.h"
 #include "display.h"
 #include "graph.h"
 
@@ -23,86 +22,6 @@ float battery_voltage() {
     return raw * 2.0 * 3.3 / 4096;
 }
 
-void get_time(WiFiClientSecure* client, String* time) {
-    *time = "UNKNOWN";
-    if (!client) {
-        return;
-    }
-    HTTPClient https;
-    const String url = "https://io.adafruit.com/api/v2/" ADAFRUIT_IO_USERNAME
-                       "/integrations/time/strftime?fmt=%25I:%25M%20%25p";
-    https.addHeader("X-AIO-Key", ADAFRUIT_IO_KEY);
-    if (https.begin(*client, url)) {
-        int httpCode = https.GET();
-        if (httpCode > 0) {
-            Serial.printf("HTTP GET... code: %d\n", httpCode);
-
-            if (httpCode == HTTP_CODE_OK ||
-                httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-                *time = https.getString();
-            }
-        } else {
-            Serial.printf("HTTPS GET failed, error: %s\n",
-                          https.errorToString(httpCode).c_str());
-        }
-        https.end();
-    } else {
-        Serial.printf("Failed to make HTTP call to %s", url.c_str());
-    }
-}
-
-size_t fetch_data(WiFiClientSecure* client, const String& feed_name,
-                  float vals[], String* name) {
-    *name = "NO DATA";
-    if (!client) {
-        return 0;
-    }
-
-    size_t val_count = 0;
-    DynamicJsonDocument doc(32768);
-
-    HTTPClient https;
-    const String url = "https://io.adafruit.com/api/v2/" ADAFRUIT_IO_USERNAME
-                       "/feeds/" +
-                       feed_name + "/data/chart?hours=168&resolution=60";
-    https.addHeader("X-AIO-Key", ADAFRUIT_IO_KEY);
-    if (https.begin(*client, url)) {
-        int httpCode = https.GET();
-        if (httpCode > 0) {
-            // HTTP header has been send and Server response header has
-            // been handled
-            Serial.printf("HTTP GET... code: %d\n", httpCode);
-
-            if (httpCode == HTTP_CODE_OK ||
-                httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-                DeserializationError error =
-                    deserializeJson(doc, https.getString());
-
-                if (error) {
-                    Serial.print("Could not parse data from HTTP request: ");
-                    Serial.println(error.c_str());
-                    return 0;
-                }
-
-                JsonArray data = doc["data"];
-                val_count = min(MAX_VALS, data.size());
-                for (size_t i = 0; i < val_count; i++) {
-                    JsonArray elem = data[i];
-                    vals[i] = elem[1].as<float>();
-                }
-                *name = doc["feed"]["name"].as<String>();
-            }
-        } else {
-            Serial.printf("HTTPS GET failed, error: %s\n",
-                          https.errorToString(httpCode).c_str());
-        }
-        https.end();
-    } else {
-        Serial.printf("Failed to make HTTP call to %s", url.c_str());
-    }
-
-    return val_count;
-}
 
 void setup() {
     Serial.begin(115200);
@@ -174,7 +93,7 @@ void loop() {
                     String name;
                     float vals[MAX_VALS];
                     size_t val_count =
-                        fetch_data(client, feed_name, vals, &name);
+                        fetch_data(client, feed_name, MAX_VALS, vals, &name);
                     draw_graph(name, x * graph_width, y * graph_height,
                                graph_width, graph_height, val_count, vals);
                 }

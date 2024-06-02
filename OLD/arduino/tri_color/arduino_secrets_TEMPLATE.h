3add045ea7b9b2ec728ee7fb167bb3ec94c37e5f
Tim Boldt <tim.boldt@gmail.com>
2024-06-02 16:31:15 -0700

    Remove legacy stuff from Rust branch

--- OLD/arduino/tri_color/arduino_secrets_TEMPLATE.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define ADAFRUIT_IO_KEY ""
-#define ADAFRUIT_IO_USERNAME ""
-#define SECRET_WIFI_SSID ""
-#define SECRET_WIFI_PASSWORD ""
--- OLD/arduino/tri_color/flash.sh
+++
@@ -1,1 +0,0 @@
-arduino-cli compile --fqbn esp32:esp32:adafruit_feather_esp32_v2 && arduino-cli upload --fqbn esp32:esp32:adafruit_feather_esp32_v2 --port /dev/ttyACM0
